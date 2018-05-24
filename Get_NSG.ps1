9e995b287110e13159b86f91608eb8f32fbf9226
Jeroen Nijssen <24685173+pompbediener@users.noreply.github.com>
2018-05-24 16:05:08 +0200

    Getting Scripts for Azure

---
+++ Get_NSG.ps1
@@ -0,0 +1,105 @@
+﻿<#
+.SYNOPSIS
+    Export NGS with Rulesets & Resource groups
+.DESCRIPTION
+    Version 0.1 for an export of all NGS's with their rulesets and corresponding Resource groups
+.NOTES
+    -----------------------------------------------------------------------------------------------------------------------------------
+    Function name : Get_NSG.ps1
+    Authors       : Jeroen Nijssen
+    Version       : 1.0
+    dependancies  : Azure - Check in Script
+    -----------------------------------------------------------------------------------------------------------------------------------
+    -----------------------------------------------------------------------------------------------------------------------------------
+    Version Changes:
+    Date: (dd-MM-YYYY)    Version:     Changed By:           Info:
+    15-05-2018            V0.1         xxxxx                 Initial script
+    24-05-2018            V1.0         Jeroen Nijssen        Finished script
+    -----------------------------------------------------------------------------------------------------------------------------------
+#>
+
+##########################
+#     Variables         #
+##########################
+
+#region Variables
+$TenantID =
+$PathtoFolder = 'C:\Temp\'
+$date = Get-Date -UFormat "%Y-%m-%d"
+#endregion Variables
+
+##########################
+#   Check need modules   #
+##########################
+ 
+if( !(Get-Module -ListAvailable -Name Azure) )
+{
+  Write-Host "Module AzureAD does not exist, installing." -ForegroundColor Yellow
+  Install-Module Azure 
+}  
+
+Else {
+Write-Host "Modules are installed" -ForegroundColor Green
+}
+
+##########################
+#     Login to Azure     #
+##########################
+
+#region login
+# Login to AzureRM account
+$OutputNull=Add-AzureAccount -Tenant $TenantID -Erroraction SilentlyContinue
+#endregion Login
+
+
+##########################
+#     Getting Info       #
+##########################
+#region Getting Subs in Tenant
+$Subscription_Name = Get-AzureSubscription | where {$_.TenantId -eq $TenantID} | Select SubscriptionName
+#endregion
+
+#############################################
+#     Looping to subs and getting NSGs      #
+#############################################
+#region Looping to Subs and getting the NSGs assoisiated to that Sub
+Foreach ($Sub_Name in $Subscription_Name)
+{ 
+    $SelectedSubscription = Select-AzureSubscription -SubscriptionName $Sub_Name.SubscriptionName
+    #Select-AzureSubscription -SubscriptionName 'PostNL Sogeti-P1'
+    #Select-AzureRmSubscription -Subscriptionid $Subid 
+    Write-Host "searching Network security groups in "$Sub_Name.SubscriptionName -Foregroundcolor Yellow
+    Write-Host "Getting NSG Rules ...." -Foregroundcolor Yellow
+        
+    Try{$NSGs = Get-AzureNetworkSecurityGroup -ErrorAction Stop}
+    Catch{ return "Failed to collect NSGs in subscription"}
+
+    $Array = @()
+
+    ForEach($NSG in $NSGs){
+        $NSGName = $NSG.name
+        $Rules = Get-AzureNetworkSecurityGroup -Name $NSGName -Detailed | Select-Object -ExpandProperty Rules | Where{!($_.IsDefault)}
+
+        ForEach($Rule in $Rules){
+            $objreturn = "" | Select NSGName,RuleName,Type,Priority,Action,SourceAddressPrefix,SourcePortRange,DestinationAddressPrefix,DestinationPortRange,Protocol
+            $objreturn.NSGName = $NSGName
+            $objreturn.RuleName = $Rule.Name
+            $objreturn.Type = $Rule.Type
+            $objreturn.Priority = $Rule.Priority
+            $objreturn.Action = $Rule.Action
+            $objreturn.SourceAddressPrefix = $Rule.SourceAddressPrefix
+            $objreturn.SourcePortRange = $Rule.SourcePortRange
+            $objreturn.DestinationAddressPrefix = $Rule.DestinationAddressPrefix
+            $objreturn.DestinationPortRange = $Rule.DestinationPortRange
+            $objreturn.Protocol = $Rule.Protocol
+
+            $Array += $objreturn
+        }
+    }
+    $SUB_NAME_File = $Sub_Name.SubscriptionName
+    $Array | Export-Csv -Path "$PathtoFolder\exportNSG-$SUB_NAME_File-$date.csv" -Delimiter ";" -Append -NoTypeInformation 
+    Write-Host "Created $PathtoFolder\exportNSG-$SUB_NAME_File-$date.csv" -ForegroundColor Green       
+}
+
+Write-Host "All done, please check $PathtoFolder" -ForegroundColor Green
+#endregion
---
+++ Get_Subnets.ps1
@@ -0,0 +1,111 @@
+﻿<#
+.SYNOPSIS
+    Export NGS with Rulesets & Resource groups
+.DESCRIPTION
+    Version 0.1 for an export of all NGS's with their rulesets and corresponding Resource groups
+.NOTES
+    -----------------------------------------------------------------------------------------------------------------------------------
+    Function name : Get_Subnets.ps1
+    Authors       : Jeroen Nijssen
+    Version       : 1.0
+    dependancies  : AzureRm - Check in Script
+    -----------------------------------------------------------------------------------------------------------------------------------
+    -----------------------------------------------------------------------------------------------------------------------------------
+    Version Changes:
+    Date: (dd-MM-YYYY)    Version:     Changed By:           Info:
+    15-05-2018            V0.1         xxxxxx                Initial script
+    24-05-2018            V1.0         Jeroen Nijssen        Finished script
+    -----------------------------------------------------------------------------------------------------------------------------------
+#>
+
+##########################
+#     Variables         #
+##########################
+
+#region Variables
+$TenantID = ""
+$PathtoFolder = 'C:\Temp\'
+$date = Get-Date -UFormat "%Y-%m-%d"
+#endregion Variables
+
+##########################
+#   Check need modules   #
+##########################
+ 
+if( !(Get-Module -ListAvailable -Name Azure) )
+{
+  Write-Host "Module AzureRm does not exist, installing." -ForegroundColor Yellow
+  Install-Module AzureRm 
+}  
+
+Else {
+Write-Host "Modules are installed" -ForegroundColor Green
+}
+
+##########################
+#     Login to Azure     #
+##########################
+
+#region login
+# Login to AzureRM account
+$OutputNull=Connect-AzureRMaccount -Tenant $TenantID -Erroraction SilentlyContinue
+#endregion Login
+
+
+##########################
+#     Getting Info       #
+##########################
+#region Getting Subs in Tenant
+$Subscription_Name = Get-AzurermSubscription | where {$_.TenantId -eq $TenantID} | Select Name,Id
+#endregion
+
+#############################################
+#     Looping to subs and getting RsGroups  #
+#############################################
+#region Looping to Subs and getting the Resourcegroup and Subnets assoisiated to that Sub
+Foreach ($Sub_Name in $Subscription_Name)
+{ 
+    $SelectedSubscription = Set-AzureRmContext -Subscription $Sub_Name.Id
+    Write-Host "Searching for Resourcegroups in "$Sub_Name.Name -Foregroundcolor Yellow
+    Write-Host "Getting Resourcegroups..." -Foregroundcolor Yellow
+        
+     $Resources = Get-AzureRMResourceGroup | Select ResourceGroupName
+    
+Foreach ($Resourcegroupname in $Resources) { 
+            $Subnets = Get-AzureRmVirtualNetwork -ResourceGroupName $Resourcegroupname.ResourceGroupName | Get-AzureRmVirtualNetworkSubnetConfig 
+            #$Subnets = Get-AzurermVirtualNetworkSubnetConfig -VirtualNetwork (Get-AzureRmVirtualNetwork -ResourceGroupName $Resourcegroupname.ResourceGroupName) 
+    if ($Subnets -eq $null){
+        Write-host "No Subnets in "$Resourcegroupname.ResourceGroupName -Foregroundcolor Red
+        $temp = $Resourcegroupname.ResourceGroupName
+        Out-File -FilePath "$PathtoFolder\NOexportSubnets-$SUB_NAME_File-$date.csv" -InputObject "No Subnets are located in $Temp" -Append -Encoding string
+    } 
+    Else{
+    $Array2 = @()
+    Write-host "Found Subnets in "$Resourcegroupname.ResourceGroupName -Foregroundcolor Yellow
+        ForEach($Subnet in $Subnets){
+   
+            $objreturn1 = "" | Select Resourcegroupname,SubnetName,AddressPrefix,NetworkSecurityGroup,RouteTable,ServiceEndpoints
+            $objreturn1.Resourcegroupname = $Resourcegroupname.ResourceGroupName
+            $objreturn1.SubnetName = $Subnet.Name
+            $objreturn1.AddressPrefix = $Subnet.AddressPrefix
+            $objreturn1.NetworkSecurityGroup = $Subnet.NetworkSecurityGroup.Id
+            $objreturn1.RouteTable = $Subnet.RouteTable
+            $objreturn1.ServiceEndpoints = $Subnet.ServiceEndpoints
+            
+            $Array2 += $objreturn1
+
+            $SUB_NAME_File = $Sub_Name.Name
+            $Array2 | Export-Csv -Path "$PathtoFolder\exportSubnets-$SUB_NAME_File-$date.csv" -Delimiter ";" -Append -NoTypeInformation 
+            Write-Host "Added info to $PathtoFolder\exportSubnets-$SUB_NAME_File-$date.csv" -ForegroundColor Green
+            }
+            }
+            }
+
+
+  
+    
+       
+}
+
+Write-Host "All done, please check $PathtoFolder" -ForegroundColor Green
+#endregion
